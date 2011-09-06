a014727e63d0a8d1ba97a39fd61bd1ee977a9b9a
John Mooring <john@mooring.com>
2011-09-05 20:30:18 -0400

    UI elements added.

--- gen/com/vtdining/R.java
+++ gen/com/vtdining/R.java
@@ -11,13 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int back=0x7f020000;
+        public static final int calendar=0x7f020001;
+        public static final int forward=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int location=0x7f020004;
+    }
+    public static final class id {
+        public static final int back=0x7f050002;
+        public static final int buttons=0x7f050001;
+        public static final int calendar=0x7f050003;
+        public static final int date=0x7f050005;
+        public static final int locationName=0x7f050000;
+        public static final int locationScroller=0x7f050006;
+        public static final int locations=0x7f050007;
+        public static final int next=0x7f050004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int location=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
     }
 }
--- src/com/vtdining/VT_Dining.java
+++ src/com/vtdining/VT_Dining.java
@@ -2,12 +2,23 @@ package com.vtdining;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class VT_Dining extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+	LinearLayout locations = (LinearLayout) findViewById(R.id.locations);
+
+	for (int i = 0; i < 10; i++) {
+	    LinearLayout location = (LinearLayout)getLayoutInflater()
+		    .inflate(R.layout.location, null);
+	    ((TextView)location.getChildAt(0)).setText("asdf "+i);
+	    locations.addView(location);
+	}
+
     }
 }
