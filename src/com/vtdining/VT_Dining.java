46e9007e35ba3f354f9a1af4f6185242e5396a6b
John Mooring <john@mooring.com>
2011-09-06 01:00:05 -0400

    Crappy but threaded tie of backend to frontend. TODO: Implement lights, date switcher, calendar picker, format hours display, tidy code.

--- src/com/vtdining/VT_Dining.java
+++ src/com/vtdining/VT_Dining.java
@@ -1,28 +1,66 @@
 package com.vtdining;
 
+import android.os.Message;
+
+import android.os.Handler;
+
+import android.os.Looper;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class VT_Dining extends Activity {
     /** Called when the activity is first created. */
+    private VT_Dining m;
+    private Day day;
+    private LinearLayout locations;
+    private TextView date;
+    private Handler h;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
-	LinearLayout locations = (LinearLayout) findViewById(R.id.locations);
-	try {
-	    Day day=new Day(5,9,2011);
-
-	    for(Location l:day.getLocations())
-	    {
-		LinearLayout location = (LinearLayout)getLayoutInflater()
-		    .inflate(R.layout.location, null);
-	    ((TextView)location.getChildAt(0)).setText(l.getName());
-	    locations.addView(location);
+	m = this;
+	date=(TextView)findViewById(R.id.date);
+	locations = (LinearLayout) findViewById(R.id.locations);
+	 h=new Handler() {
+	    public void handleMessage(Message m) {
+		setUp();
 	    }
-	}catch(Exception e) {}
+	};
+	load();
+    }
 
+    public void load() {
+	new Thread() {
+	    public void run() {
+		Looper.prepare();
+		try {
+		    day = new Day(5, 9, 2011);
+		    h.sendEmptyMessage(0);
+		} catch (Exception e) {
+		    Toast.makeText(m, "35 " + e, Toast.LENGTH_LONG).show();
+		}
+	    }
+	}.start();
+    }
+
+    public void setUp() {
+	try {
+	    for (Location l : day.getLocations()) {
+		ViewGroup location = (ViewGroup) getLayoutInflater().inflate(
+			R.layout.location, null);
+		((TextView) location.getChildAt(0)).setText(l.getName());
+		((TextView) location.getChildAt(1)).setText(l.getTimes());
+		locations.addView(location);
+		location.invalidate();
+	    }
+	} catch (Exception e) {
+	    Toast.makeText(this, "" + e, Toast.LENGTH_LONG).show();
+	}
     }
 }
