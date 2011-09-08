fcfedfb83b0feb305762cb576c2abb7d9b172f4b
John Mooring <john@mooring.com>
2011-09-08 00:17:26 -0400

    Removed massive database.

--- gen/com/vtdining/R.java
+++ gen/com/vtdining/R.java
@@ -23,24 +23,21 @@ public final class R {
         public static final int refresh=0x7f020009;
     }
     public static final class id {
-        public static final int back=0x7f060003;
-        public static final int buttons=0x7f060002;
-        public static final int calendar=0x7f060004;
-        public static final int date=0x7f060006;
-        public static final int locationName=0x7f060000;
-        public static final int locationScroller=0x7f060007;
-        public static final int locations=0x7f060008;
-        public static final int next=0x7f060005;
-        public static final int textView1=0x7f060001;
+        public static final int back=0x7f050003;
+        public static final int buttons=0x7f050002;
+        public static final int calendar=0x7f050004;
+        public static final int date=0x7f050006;
+        public static final int locationName=0x7f050000;
+        public static final int locationScroller=0x7f050007;
+        public static final int locations=0x7f050008;
+        public static final int next=0x7f050005;
+        public static final int textView1=0x7f050001;
     }
     public static final class layout {
         public static final int location=0x7f030000;
         public static final int main=0x7f030001;
     }
-    public static final class raw {
-        public static final int data=0x7f040000;
-    }
     public static final class string {
-        public static final int app_name=0x7f050000;
+        public static final int app_name=0x7f040000;
     }
 }
